e654adf36ac80e49db7fde89a1b73f4c7cdd132f
Arjun <dahiyaarjun343@gmail.com>
2024-01-21 23:20:47 +0530

    uncomment

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               Text('Wallpaper'),
               Text('preview'),
-              // Image.asset('assets/images/peakpx 3.jpg')
+              Image.asset('assets/images/peakpx 3.jpg')
             ]
           ),
         ),
