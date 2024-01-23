cc7e0e4c0aff61d9df42f224a0505f3f7400bcf6
AkshitChaudhary123 <akshitchaudhary913@gmail.com>
2024-01-23 20:49:40 +0530

    container

---
+++ lib/practice.dart
@@ -0,0 +1,23 @@
+
+import 'dart:html';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class Ap{
+void maiin(){
+
+  // stdout.write("enter name");
+  // var a=stdin.readLineSync();
+  // stdout.write("welcome $a");
+  body:Center(
+  child:
+  Container(
+    color: Color.fromARGB(255, 172, 138, 39),
+    width: 500,
+    child: Text('sdfghj'),
+  )
+  );
+
+}
+}
