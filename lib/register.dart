e373a41719c95f432c73d9a5dfa5780daa7bc328
AkshitChaudhary123 <akshitchaudhary913@gmail.com>
2024-02-04 22:22:22 +0530

    modification updates

--- lib/register.dart
+++ lib/register.dart
@@ -15,42 +15,68 @@ class _UserRegisterState extends State<UserRegister> {
         // mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Row(
-            // mainAxisAlignment: MainAxisAlignment.center,
+             mainAxisAlignment: MainAxisAlignment.center,
             //crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Text('Register Page',
-                  style: TextStyle(color: Colors.black, fontSize: 50)),
+                  style: TextStyle(color: Colors.black, fontSize: 30,backgroundColor: Colors.yellowAccent)),
             ],
           ),
+          const SizedBox(height: 50,),
           Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Text('Welcome\nTo Demo App',
-                  style: TextStyle(color: Colors.blue, fontSize: 50)),
+                  style: TextStyle(color: Colors.blue, fontSize: 30)),
             ],
           ),
+            const SizedBox(height: 30),
               TextField(
                   decoration: InputDecoration(
+                    
                       hintText: 'Email',
+                      
                       fillColor: Colors.yellow,
                       filled: true,
+                      prefixIcon: Icon(Icons.email,color: Colors.redAccent),
                       focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.blue)),
+                          borderSide: BorderSide(color: const Color.fromARGB(255, 243, 89, 33)),borderRadius: BorderRadius.circular(50)),
                       border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10)))),
+                          borderRadius: BorderRadius.circular(20)))),
+                        const SizedBox(height: 20),  
               TextField(
+                obscureText: true,
+                obscuringCharacter: "*",
                   decoration: InputDecoration(
+                    
                       hintText: 'Password',
+                      prefixIcon: Icon(Icons.password,color: Colors.redAccent),
                       fillColor: Colors.yellow,
                       filled: true,
                       focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.blue)),
+                          borderSide: BorderSide(color: Colors.blue),borderRadius: BorderRadius.circular(50)),
                       border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10)))),
+                          borderRadius: BorderRadius.circular(20)))),
 
+                const SizedBox(height: 20),
+                          TextField(
+                obscureText: true,
+                obscuringCharacter: "*",
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.password,color: Colors.redAccent),
+                   // suffixIcon: IconButton(icon: Icon()),
+                      hintText: 'Confirm Password',
+                      
+                      fillColor: Colors.yellow,
+                      filled: true,
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),borderRadius: BorderRadius.circular(50)),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20)))),
 
-              ElevatedButton(onPressed: () {}, child: Text('Register')),
-
+              const SizedBox(height: 40),
+              ElevatedButton(onPressed: () {}, child: Text('Register',style: TextStyle(color: Colors.green)),
+              )
         ],
       ),
     );
