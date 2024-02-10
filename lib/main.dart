a1a4fb481a1a208180744bd56a219b89005487bb
Arjun <dahiyaarjun343@gmail.com>
2024-02-10 15:31:59 +0530

    practices

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
-import 'package:first_project_flutter/login.dart';
-import 'package:first_project_flutter/model_user_login.dart';
-import 'package:first_project_flutter/practice.dart';
-import 'package:first_project_flutter/register.dart';
+import 'package:first_project_flutter/home/home.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: 'login',
-    routes: {
-      'login': (context)=>UserLogin(),
-      'register': (context)=>UserRegister(),
-      'practice': (context)=>Practice(),
-      // 'model_user_login': (context)=>ModelUserLogin(),
-    },
-  ));
+ void main(){
+  runApp(MyApp());
+ }
+
+ class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
 }
