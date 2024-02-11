f21308abea340c06a35c28fb760ae7eb2b6af272
Arjun <dahiyaarjun343@gmail.com>
2024-02-11 23:13:35 +0530

    updates

--- lib/login.dart
+++ lib/login.dart
@@ -4,12 +4,15 @@ import "package:flutter/material.dart";
 class UserLogin extends StatefulWidget {
   const UserLogin({super.key});
 
+
   @override
   State<UserLogin> createState() => _UserLoginState();
 }
 
 class _UserLoginState extends State<UserLogin> {
   apiResponse data=apiResponse();
+  TextEditingController emailC = TextEditingController();
+  TextEditingController passwordC = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return 
@@ -21,25 +24,20 @@ class _UserLoginState extends State<UserLogin> {
          backgroundColor: Colors.transparent,
         body: Stack(
           children: [
-            // Container(
-            //   padding: EdgeInsets.only(left: 22,top: 100),
-            //   // child: Text('Welcome\nTo Demo App',
-            //   child: FutureBuilder<List>(
-            //     future: data.getResponse('dahiyaarjun343@gmail.com','@Arjun343'), 
-            //     builder: (context,snapshot){
-            //       print(snapshot);
-            //       return Text('arjun');
-            //     }
-            //   // ),
-            //     // style: TextStyle(color: Colors.blue,fontSize: 33),
-            //   ),
-            // ),
+            Container(
+              padding: EdgeInsets.only(left: 22,top: 100),
+              child: Text('Welcome\nTo Demo App',
+              // ),
+                style: TextStyle(color: Colors.blue,fontSize: 33),
+              ),
+            ),
             SingleChildScrollView(
                 child:Container(
                   padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.4,left: 35,right: 35),
                   child: Column(
                     children: [
                       TextField(
+                        controller: emailC,
                         decoration: InputDecoration(
                           hintText: 'Email',
                           fillColor: Colors.amber.shade100,
@@ -53,6 +51,7 @@ class _UserLoginState extends State<UserLogin> {
                         height: 25,
                       ),
                       TextField(
+                        controller: passwordC,
                         obscureText: true,
                         decoration: InputDecoration(
                           hintText: 'Password',
@@ -77,7 +76,12 @@ class _UserLoginState extends State<UserLogin> {
                               radius: 30,
                               backgroundColor: Colors.amber,
                               child: IconButton(
-                                onPressed: (){}, 
+                                onPressed: (){
+                                  String email=emailC.text.toString();
+                                  String password=emailC.text.toString();
+                                  apiResponse loginApi=apiResponse();
+                                  loginApi.getResponse(email,password);
+                                }, 
                                 icon: Icon(Icons.arrow_forward)),
                             )
                         ],
