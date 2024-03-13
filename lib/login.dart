c06e705b4b81eb546f434c4108303e97a1d2ebd8
dahiyaarjun <108714643+dahiyaarjun@users.noreply.github.com>
2024-03-13 16:51:17 +0530

    updates

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,7 @@
 import "package:first_project_flutter/backend/laravel.dart";
 import "package:first_project_flutter/model_user_login.dart";
 import "package:flutter/material.dart";
+import "package:get/get.dart";
 
 class UserLogin extends StatefulWidget {
   const UserLogin({super.key});
@@ -16,111 +17,119 @@ class _UserLoginState extends State<UserLogin> {
   TextEditingController passwordC = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-    body: Container(
-
-    child: Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(image: AssetImage('assets/images/peakpx 3.jpg'), fit: BoxFit.cover)
-      ),
-      child: Scaffold(
-         backgroundColor: Colors.transparent,
-        body: Stack(
-          children: [
-            Container(
-              padding: EdgeInsets.only(left: 22,top: 100),
-              child: Text('Welcome\nTo Demo App',
-              // ),
-                style: TextStyle(color: Colors.blue,fontSize: 33),
+    return GetMaterialApp(
+      title: 'Demo App',
+      home: Scaffold(
+        appBar: AppBar(title: Text('Demo APP')),
+      body: Container(
+      
+      child: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(image: AssetImage('assets/images/peakpx 3.jpg'), fit: BoxFit.cover)
+        ),
+        child: Scaffold(
+          //  backgroundColor: Colors.transparent,
+          body: Stack(
+            children: [
+              Container(
+                padding: EdgeInsets.only(left: 22,top: 100),
+                child: Text('Welcome\nTo Demo App',
+                // ),
+                  style: TextStyle(color: Colors.blue,fontSize: 33),
+                ),
               ),
-            ),
-            SingleChildScrollView(
-                child:Container(
-                  padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.4,left: 35,right: 35),
-                  child: Column(
-                    children: [
-                      TextField(
-                        controller: emailC,
-                        decoration: InputDecoration(
-                          hintText: 'Email',
-                          fillColor: Colors.amber.shade100,
-                          filled: true,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8)
-                          )
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 25,
-                      ),
-                      TextField(
-                        controller: passwordC,
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          hintText: 'Password',
-                          fillColor: Colors.amber.shade100,
-                          filled: true,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8)
-                          )
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 40,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('Sign In',style: TextStyle(
-                            color: Colors.amber,fontSize: 40,
-                            fontWeight: FontWeight.bold,
-                            ),),
-                            CircleAvatar(
-                              radius: 30,
-                              backgroundColor: Colors.amber,
-                              child: IconButton(
-                                onPressed: (){
-                                  String email=emailC.text.toString();
-                                  String password=passwordC.text.toString();
-                                        FutureBuilder(future: data.getResponse(email,password), builder: (BuildContext context, AsyncSnapshot<ModelUserLogin>snapshot) {
-                                        if(snapshot.hasData){
-                                          print("success");
-                                          return Container(
-                                            height: 100,
-                                            width: 100,
-                                            color: Colors.blue,
-                                            child: Text("Success")
-                                            );
-                                        }else{
-                                          return Text("error");
-                                        }
-                                      });
-                                }, 
-                                icon: Icon(Icons.arrow_forward)),
+              SingleChildScrollView(
+                  child:Container(
+                    padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.4,left: 35,right: 35),
+                    child: Column(
+                      children: [
+                        TextField(
+                          controller: emailC,
+                          decoration: InputDecoration(
+                            hintText: 'Email',
+                            fillColor: Colors.amber.shade100,
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8)
                             )
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          ElevatedButton(onPressed: (){
-                            Navigator.pushNamed(context, 'register');
-                          }, child: Text('SIGNUP')
                           ),
-                          ElevatedButton(onPressed: (){
-                            Navigator.pushNamed(context, 'practice');
-                          }, child: Text('practice')
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
+                        ),
+                        const SizedBox(
+                          height: 25,
+                        ),
+                        TextField(
+                          controller: passwordC,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            hintText: 'Password',
+                            fillColor: Colors.amber.shade100,
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8)
+                            )
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Sign In',style: TextStyle(
+                              color: Colors.amber,fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                              ),),
+                              CircleAvatar(
+                                radius: 30,
+                                backgroundColor: Colors.amber,
+                                child: IconButton(
+                                  onPressed: (){
+                                    String email=emailC.text.toString();
+                                    String password=passwordC.text.toString();
+                                          FutureBuilder(future: data.getResponse(email,password), builder: (BuildContext context, AsyncSnapshot<ModelUserLogin>snapshot) {
+                                          if(snapshot.hasData){
+                                            print("success");
+                                            return Container(
+                                              height: 100,
+                                              width: 100,
+                                              color: Colors.blue,
+                                              child: Text("Success")
+                                              );
+                                          }else{
+                                            return Text("error");
+                                          }
+                                        });
+                                  }, 
+                                  icon: Icon(Icons.arrow_forward)),
+                              )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            ElevatedButton(onPressed: (){
+                              Navigator.pushNamed(context, 'register');
+                            }, child: Text('SIGNUP')
+                            ),
+                            // ElevatedButton(onPressed: (){
+                            //   Get.snackbar('snack', 'snack successfully');
+                            // }, child: Text('show snackbar')
+                            // ),
+                            // ElevatedButton(onPressed: (){
+                            //   Get.defaultDialog();
+                            // }, child: Text('Alert')
+                            // )
+                          ],
+                        ),
+                      ],
+                    ),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
-    ),
-    ),
+      ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
+import 'package:first_project_flutter/app/modules/home/view.dart';
 import 'package:first_project_flutter/login.dart';
 import 'package:first_project_flutter/model_user_login.dart';
 import 'package:first_project_flutter/practice.dart';
 import 'package:first_project_flutter/register.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: 'login',
-    routes: {
-      'login': (context)=>UserLogin(),
-      'register': (context)=>UserRegister(),
-      'practice': (context)=>Practice(),
-      // 'model_user_login': (context)=>ModelUserLogin(),
-    },
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      title: 'Todo List using GetX',
+      home: HomePage(),
+    );
+  }
 }
