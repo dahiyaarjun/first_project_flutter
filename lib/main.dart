fbf39d0e7894fd2cbdb160f19af9518b51d4867e
dahiyaarjun <108714643+dahiyaarjun@users.noreply.github.com>
2024-03-14 17:18:29 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:first_project_flutter/login.dart';
 import 'package:first_project_flutter/model_user_login.dart';
 import 'package:first_project_flutter/practice.dart';
 import 'package:first_project_flutter/register.dart';
-import 'package:first_project_flutter/reset_pass.dart';
+// import 'package:first_project_flutter/reset_pass.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +13,7 @@ void main() {
       'login': (context)=>UserLogin(),
       'register': (context)=>UserRegister(),
       'practice': (context)=>Practice(),
-      'ResetPassword': (context)=>ResetPassword(),
+      // 'ResetPassword': (context)=>ResetPassword(),
       // 'model_user_login': (context)=>ModelUserLogin(),
     },
   ));
