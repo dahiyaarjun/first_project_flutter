19f2106cfe9f85f585c6b57bb707fc8e086a7565
dahiyaarjun <108714643+dahiyaarjun@users.noreply.github.com>
2024-03-14 17:18:17 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,20 @@
-import 'package:first_project_flutter/app/modules/home/view.dart';
 import 'package:first_project_flutter/login.dart';
 import 'package:first_project_flutter/model_user_login.dart';
 import 'package:first_project_flutter/practice.dart';
 import 'package:first_project_flutter/register.dart';
+import 'package:first_project_flutter/reset_pass.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      title: 'Todo List using GetX',
-      home: HomePage(),
-    );
-  }
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: 'login',
+    routes: {
+      'login': (context)=>UserLogin(),
+      'register': (context)=>UserRegister(),
+      'practice': (context)=>Practice(),
+      'ResetPassword': (context)=>ResetPassword(),
+      // 'model_user_login': (context)=>ModelUserLogin(),
+    },
+  ));
 }
