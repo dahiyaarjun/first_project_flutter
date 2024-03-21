8a74b12446f0b4055db59b65522b2cad309e8ebe
AkshitChaudhary123 <akshitchaudhary913@gmail.com>
2024-03-21 21:48:09 +0530

    searchPage added

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:first_project_flutter/pages/newpassword.dart';
 import 'package:first_project_flutter/pages/practice.dart';
 import 'package:first_project_flutter/pages/register.dart';
 import 'package:first_project_flutter/pages/resetPassword.dart';
+import 'package:first_project_flutter/pages/searchchat.dart';
 // import 'package:first_project_flutter/reset_pass.dart';
 import 'package:flutter/material.dart';
  
@@ -19,6 +20,7 @@ void main() {
       'register': (context)=>const UserRegister(),
       'practice': (context)=>const Practice(),
       'ResetPassword': (context)=>const ResetPassword(),
+      'Search': (context)=>const Search(),
       // 'NewPassword':(context) => const NewPassword(),
       // 'model_user_login': (context)=>ModelUserLogin(),
     }
