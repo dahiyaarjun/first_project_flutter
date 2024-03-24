df13c625aa2ae938c07a7310cfb46d5bcc2e0a47
AkshitChaudhary123 <akshitchaudhary913@gmail.com>
2024-03-24 22:19:56 +0530

    merges

--- lib/pages/searchchat.dart
+++ lib/pages/searchchat.dart
@@ -13,8 +13,14 @@ class Search extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<Search> {
+<<<<<<< Updated upstream
   final TextEditingController _message = TextEditingController();
 
+=======
+  TextEditingController _message=TextEditingController();
+  
+  
+>>>>>>> Stashed changes
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,6 +54,7 @@ class _MyWidgetState extends State<Search> {
                           repeat: true,
                           fit: BoxFit.cover);
 
+<<<<<<< Updated upstream
                     case ConnectionState.done:
                       if (snapshot.hasError) {
                         return Text('${snapshot.error}');
@@ -73,6 +80,46 @@ class _MyWidgetState extends State<Search> {
                               )),
                             ),
                           ),
+=======
+      body:Container(
+      
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          
+          // height: 500,
+          color: Colors.black,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                
+             
+                 
+                    FutureBuilder(future: apiResponse.apiSearch(context: context, message: "cow essay in 200 lines"),
+                   builder: (BuildContext context,AsyncSnapshot<SearchModel>snapshot){
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                        
+                        return CircularProgressIndicator();
+                       } else if (snapshot.hasError) {
+                        
+                        return Text('Error: ${snapshot.error}');
+                      } else {
+                        // If data is successfully loaded, display the fetched data
+                        print(snapshot.data?.content);
+                        return Expanded(child:
+                               SingleChildScrollView(
+                               
+                               child:Center(
+                               child: Container(
+                                padding: EdgeInsets.only(left: 40,right: 40),
+                                child: Text('${snapshot.data?.content}',style: TextStyle(color: Colors.white,fontSize: 18),)
+                               ),
+                               
+                                       
+                               
+                               ),
+                        ),
+>>>>>>> Stashed changes
                         );
                       } else {
                         return const Text('NO DATA FOUND');
@@ -89,6 +136,7 @@ class _MyWidgetState extends State<Search> {
                 ),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+<<<<<<< Updated upstream
                   children: [
                     Expanded(
                       child: Container(
@@ -132,6 +180,71 @@ class _MyWidgetState extends State<Search> {
                             icon: const Icon(Icons.send_sharp)),
                       ),
                     ),
+=======
+                  children:[
+                
+                
+                Expanded(
+                  
+                
+                
+                child:Container(
+                  margin: EdgeInsets.symmetric(horizontal: 8.0),
+                  child: TextField(
+                    controller: _message,
+                      
+                    decoration: InputDecoration(
+                                    hintText: 'How can I help you?',
+                                    filled: true,
+                                    fillColor: Colors.white,
+                                    focusedBorder: const OutlineInputBorder(
+                                        borderSide: BorderSide(color: Color(0xff1a80e5))),
+                                    hintStyle: const TextStyle(color: Color(0xff788998)),
+                                    enabledBorder: const OutlineInputBorder(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(10.0)),
+                                        borderSide: BorderSide(
+                                            color: Color(0xffe8ecf1))),
+                                    contentPadding: EdgeInsets.symmetric(
+                                        horizontal: MediaQuery.of(context)
+                                                .size
+                                                .width *
+                                            0.025,
+                                        vertical: MediaQuery.of(context)
+                                                .size
+                                                .height *
+                                            0.018),
+                                          
+                                    border: const OutlineInputBorder(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(10.0)))),
+                    
+                      
+                  ),
+                ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 2.0),
+                  child: CircleAvatar(
+                    radius: 20,
+                    backgroundColor: Colors.amber[400],
+                    
+                    child: IconButton(onPressed: (){
+                            setState(() {
+      
+                            });
+                    },
+                    icon: Icon(Icons.send_sharp)),
+                    
+                    
+                      
+                  ),
+                ),
+                
+                  
+                    
+                // ),
+>>>>>>> Stashed changes
                   ],
                 ),
               ),
