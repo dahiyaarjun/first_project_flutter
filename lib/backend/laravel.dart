0fa18dcbab59c4f815f4b280fa2853c371cacf96
dahiyaarjun <108714643+dahiyaarjun@users.noreply.github.com>
2024-04-12 16:43:12 +0530

    deleted extra

--- lib/backend/laravel.dart
+++ lib/backend/laravel.dart
@@ -1,61 +1,29 @@
 import 'package:first_project_flutter/backend/sharedPreference.dart';
 import 'package:first_project_flutter/custom_helper/constants.dart';
-import 'package:first_project_flutter/models/LoginDetails_model.dart';
 import 'package:first_project_flutter/models/chapter_model';
-import 'package:first_project_flutter/models/dummy_model.dart';
-import 'package:first_project_flutter/models/registermodel.dart';
 import 'package:first_project_flutter/models/responsemodel.dart';
-import 'package:first_project_flutter/models/loginmodel.dart';
 import 'package:first_project_flutter/models/search_model.dart';
 import 'package:first_project_flutter/models/subjectmodel.dart';
-import 'package:first_project_flutter/pages/practice.dart';
-import 'package:first_project_flutter/pages/searchchat.dart';
 import 'package:first_project_flutter/widgets/utils.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
-// ignore: camel_case_types
-
-
-
 class apiResponse {
-
   static String baseUrl = AppConstants.baseUrl;
-
   static Future<String> apiUserDetails({required String email}) async{
-
     try {
-     
       String apiUrl = '${baseUrl}api/user/details';
-      print('apiHit');
-      var Response = await http.post(
-        Uri.parse(apiUrl),
-        body: {
-          'email':"dahiyaarjun343@gmail.com"
-          
-        },
-      );
+      var Response = await http.post(Uri.parse(apiUrl),body: {'email':email},);
       if(Response.statusCode==200){
        Map<String, dynamic> userData = jsonDecode(Response.body);
-        
-        print('arjun');
-
-        print(userData['User Details'][0]['name']);
         String name=userData['User Details'][0]['name'];
-       
         return name;
-      }
-      
-      
+      }   
     } catch (e) {
-      // Utils.showSnackBar(context, e.toString());
       return Future.error(e.toString());
     }
     return Future.error("error");
-
-
   }
 
   static Future<SearchModel> apiSearch(
@@ -63,13 +31,8 @@ class apiResponse {
     try {
       if (message.isEmpty != true) {
         String apiUrl = "${baseUrl}api/user/open-ai";
-        var Response = await http.post(
-          Uri.parse(apiUrl),
-          body: {'content': message},
-        );
+        var Response = await http.post(Uri.parse(apiUrl),body: {'content': message},);
         if (Response.statusCode == 200) {
-          // Map<String,dynamic>userData=jsonDecode(Response.body);
-          // String successful = userData['content'];
           var jsonResponse = json.decode(Response.body);
           return SearchModel.fromJson(jsonResponse);
         }
@@ -82,27 +45,13 @@ class apiResponse {
   }
 
   static Future<void> apiNewPassword(
-      {required BuildContext context,
-      required String email,
-      required String password,
-      required String password_confirmation,
-      required String otp}) async {
+      {required BuildContext context,required String email,required String password,required String password_confirmation,required String otp}) async {
     try {
       String apiUrl = '${baseUrl}api/user/reset/by-otp';
-      var Response = await http.post(
-        Uri.parse(apiUrl),
-        body: {
-          'email': email,
-          'password': password,
-          'password_confirmation': password_confirmation,
-          'otp': otp
-        },
-      );
-
+      var Response = await http.post(Uri.parse(apiUrl),body: {'email': email,'password': password,'password_confirmation': password_confirmation,'otp': otp},);
       if (Response.statusCode == 200) {
         Map<String, dynamic> userData = jsonDecode(Response.body);
         String successful = userData['message'];
-
         Utils.showSnackBar(context, successful);
       } else {
         Map<String, dynamic> errorMessage = jsonDecode(Response.body);
@@ -121,10 +70,7 @@ class apiResponse {
   }) async {
     try {
       String apiUrl = '${baseUrl}api/user/login';
-      http.Response response = await http.post(
-        Uri.parse(apiUrl),
-        body: {'email': email, 'password': password},
-      );
+      http.Response response = await http.post(Uri.parse(apiUrl),body: {'email': email, 'password': password},);
       if (response.statusCode == 200) {
         Map<String, dynamic> userData = jsonDecode(response.body);
         String msg = userData['message'];
@@ -147,23 +93,10 @@ class apiResponse {
     }
   }
 
-  Future<void> apiUserRegistration(
-      {required BuildContext context,
-      required String name,
-      required String email,
-      required String password,
-      required String password_confirmation}) async {
+  Future<void> apiUserRegistration({required BuildContext context,required String name,required String email,required String password,required String password_confirmation}) async {
     String apiUrl = '${baseUrl}api/user/register';
     try {
-      var response = await http.post(
-        Uri.parse(apiUrl),
-        body: {
-          "name": name,
-          "email": email,
-          "password": password,
-          "password_confirmation": password_confirmation,
-        },
-      );
+      var response = await http.post(Uri.parse(apiUrl),body: {"name": name,"email": email,"password": password,"password_confirmation": password_confirmation,},);
       if (response.statusCode == 200) {
         Map<String, dynamic> userData = jsonDecode(response.body);
         String message = userData['message'];
@@ -182,23 +115,16 @@ class apiResponse {
     }
   }
 
-  Future<void> apiResetPassword({
-    required BuildContext context,
-    required String email,
+  Future<void> apiResetPassword({required BuildContext context,required String email,
   }) async {
     try {
       String apiUrl = '${baseUrl}api/user/password-reset';
-      http.Response response = await http.post(
-        Uri.parse(apiUrl),
-        body: {'email': email},
-      );
+      http.Response response = await http.post(Uri.parse(apiUrl),body: {'email': email},);
       if (response.statusCode == 200) {
         Map<String, dynamic> userData = jsonDecode(response.body);
         String successful = userData['message'];
         String token = userData['token'];
         int otp = userData['otp'];
-        // print('Access Token come during Login Time' + accessToken);
-        // await SharedPreferencesHelper.setAccessToken(accessToken);
         await Utils.showSnackBar(context, successful);
       } else {
         Map<String, dynamic> errorMessage = jsonDecode(response.body);
@@ -210,36 +136,13 @@ class apiResponse {
     }
   }
 
-  Future<Autogenerated> getFact() async {
-    String url = "https://catfact.ninja/fact";
-
-    print("chlgya");
-    final response = await http.get(Uri.parse(url));
-    var data = json.decode(response.body);
-    print("bad ka chlgya");
-
-    if (response.statusCode == 200) {
-      var jsonResponse = json.decode(response.body);
-      print(jsonResponse);
-      return Autogenerated.fromJson(jsonResponse);
-      // print(jsonResponse);
-    } else {
-      print('server error');
-      return Future.error("glt");
-    }
-    // return obj;
-  }
-
   static Future<List<ResponseModel>> fetchData() async {
-    // Replace this URL with your API endpoint
     final response = await http.get(Uri.parse(
         'https://violent-wall-production.up.railway.app/api/user/all'));
     if (response.statusCode == 200) {
-      // If the call to the server was successful, parse the JSON
       final data = json.decode(response.body) as List<dynamic>;
       return data.map((json) => ResponseModel.fromJson(json)).toList();
     } else {
-      // If that call was not successful, throw an error.
       throw Exception('Failed to load data');
     }
   }
@@ -260,49 +163,29 @@ class apiResponse {
   }
 
   static Future<void> removeImage(String email) async{
-    
     try {
-  String apiUrl='${baseUrl}api/user/remove-img';
-    
-    var Response = await http.post(
-      Uri.parse(apiUrl),
-      body: {
-        "email":email
-      },
-    );
-  
+    String apiUrl='${baseUrl}api/user/remove-img';
+    var Response = await http.post(Uri.parse(apiUrl),body:{"email":email},);
     if (Response.statusCode == 200) {
       Map<String, dynamic> userData = jsonDecode(Response.body);
       String message= userData['message'];
-      if(message=="successful"){
-        print("remove kr dia");
-      }
-  
-      
     } else {
       Map<String, dynamic> errorMessage = jsonDecode(Response.body);
       String error = errorMessage['message'];
-      // Utils.showSnackBar(context, error);
     }
-} on Exception catch (e) {
-  print(e.toString());
-  // TODO
-}
-    
-    
+    } on Exception catch (e) {
+      print(e.toString());
     }
+  }
 
-    static Future<List<subjectmodel>> getSubject(String Class) async {
+  static Future<List<subjectmodel>> getSubject(String Class) async {
     String Url='${baseUrl}api/user/get-subjects';
-    final response = await http.post(Uri.parse(
-        Url),
+    final response = await http.post(Uri.parse(Url),
          body: { "class":Class
               //  "branch":null
-               },
+              },
         );
-         
     if (response.statusCode == 200) {
-      // If the call to the server was successful, parse the JSON
       final data = json.decode(response.body) as List<dynamic>;
       return data.map((json) => subjectmodel.fromJson(json)).toList();
     } else {
@@ -312,18 +195,8 @@ class apiResponse {
 
   static Future<List<chapterModel>> getChapters(String _class,String _branch,String _subject) async {
     String Url='${baseUrl}api/user/get-chapters';
-    final response = await http.post(Uri.parse(
-        Url),
-         body: { 
-                  "class": _class,
-                  "subject": _subject,
-                  "branch": _branch
-              
-               },
-        );
-         
+    final response = await http.post(Uri.parse(Url),body:{"class": _class,"subject": _subject,"branch": _branch},);  
     if (response.statusCode == 200) {
-      // If the call to the server was successful, parse the JSON
       final data = json.decode(response.body) as List<dynamic>;
       return data.map((json) => chapterModel.fromJson(json)).toList();
     } else {
@@ -332,38 +205,20 @@ class apiResponse {
   }
 
   static Future<void> updateName(String email,String name) async{
-    
     try {
-  String apiUrl='${baseUrl}api/user/update-name';
-    
-    var Response = await http.post(
-      Uri.parse(apiUrl),
-     body:  {
-    "email":email,
-     "name":name
-    },
-    );
-  
+    String apiUrl='${baseUrl}api/user/update-name';
+    var Response = await http.post(Uri.parse(apiUrl),body:{"email":email,"name":name},);
     if (Response.statusCode == 200) {
       Map<String, dynamic> userData = jsonDecode(Response.body);
       String message= userData['message'];
-      if(message=="successful"){
-        print("name updated");
-      }
-  
-      
     } else {
       Map<String, dynamic> errorMessage = jsonDecode(Response.body);
       String error = errorMessage['message'];
-      // Utils.showSnackBar(context, error);
     }
-} on Exception catch (e) {
-  print(e.toString());
-  // TODO
-}
-    
-    
+    } on Exception catch (e) {
+    print(e.toString());
     }
   }
+}
   
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,25 @@
-
 import 'package:first_project_flutter/backend/sharedPreference.dart';
 import 'package:first_project_flutter/pages/login.dart';
-import 'package:first_project_flutter/models/loginmodel.dart';
-import 'package:first_project_flutter/pages/loginGpt.dart';
-import 'package:first_project_flutter/pages/newpassword.dart';
-import 'package:first_project_flutter/pages/practice.dart';
-import 'package:first_project_flutter/pages/profileSettings.dart';
 import 'package:first_project_flutter/pages/register.dart';
 import 'package:first_project_flutter/pages/resetPassword.dart';
 import 'package:first_project_flutter/pages/searchchat.dart';
-import 'package:first_project_flutter/pages/splash.dart';
 import 'package:first_project_flutter/pages/standard.dart';
-import 'package:first_project_flutter/pages/subject.dart';
 import 'package:flutter/material.dart';
  
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  // Check if token is available
   String? token = await SharedPreferencesHelper.getAccessToken();
-   print(token);
   runApp(MaterialApp(
-    
     debugShowCheckedModeBanner: false,
     initialRoute: token != null ? 'Search' : 'login',
     // initialRoute:'/',
     routes: {
       // '/':(context)=>const splashScreen(),
       'login': (context) => const UserLogin(),
-      'login_gpt': (context) => const LoginScreen(),
       'register': (context) => const UserRegister(),
-      'practice': (context) => const Practice(),
       'ResetPassword': (context) => const ResetPassword(),
       'Search': (context) =>  Search(), 
       'standard':(token)=> standard(),
-      
-      // Add more routes as needed
     },
   ));
 }
